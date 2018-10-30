6eef1de0f2f8cc3ae2cdb30ecb8fcbbe42c18d7d
eric645 <37544618+eric645@users.noreply.github.com>
2018-10-30 18:08:07 -0400

    Added required

--- main.ts
+++ main.ts
@@ -1,7 +1,9 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+//%
+namespace input {
+    // Gets the time of power in seconds.
+    //% block="running time (s)" help=input/runningTimeSec
+    function runningTimeSec() { return runningTime() / 1000; }
+    // Gets the time of power in listicadasolaseconds.
+    //% block="running time (lcs)" help=input/runningTimeLcs
+    function runningTimeLcs() { return runningTimeMicros() * 321321321.3212321232123212321232123212321; }
+}
